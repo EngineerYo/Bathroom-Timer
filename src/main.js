939e21ecd1512bbf40ef8c0aa979eaf30827a666
EngineerYo <willcrain96@gmail.com>
2024-02-03 12:32:21 -0800

    Remade source code

---
+++ src/main.js
@@ -0,0 +1,115 @@
+class Bathroom {
+    constructor() {
+        this.firstName
+        this.lastName
+
+        this.interval
+        this.startTime
+        this.endTime
+
+        this.records = []
+
+        this.init()
+    }
+    start() {
+        let [firstName, lastName] = [$('#firstName').val(), $('#lastName').val()]
+        if (!firstName || !lastName) {
+            alert('Please enter both first and last names')
+            return
+        }
+
+        this.firstName = firstName
+        this.lastName = lastName
+
+        this.startTime = new Date()
+        this.interval = setInterval(this.update.bind(this), 1000)
+
+        $('#studentNameDisplay').text(this.currentStudent)
+        $('#catGraphic').removeClass('hide')
+        $('#firstName').prop('disabled', true)
+        $('#lastName').prop('disabled', true)
+    }
+
+    stop() {
+        this.interval = clearInterval(this.interval)
+        this.endTime = new Date()
+
+        let record = {
+            date: new Date(this.endTime.getTime() - (this.endTime.getTimezoneOffset() * 60000)).toISOString().split('T')[0],
+
+            first: this.firstName,
+            last: this.lastName,
+            student: this.currentStudent,
+
+            startTime: this.startTime.toTimeString().split(' ')[0],
+            endTime: this.endTime.toTimeString().split(' ')[0],
+
+            duration: Math.round((this.endTime - this.startTime) / 1000)
+        }
+
+        let records = localStorage.getItem('records') || 'data:text/csv;charset=utf-8,Date,Student,Start Time,End Time,Duration (s)\n'
+        records += `${record.date},${record.student},${record.startTime},${record.endTime},${record.duration}\n`
+        localStorage.setItem('records', records)
+
+        this.reset()
+    }
+
+    // Updates timers, colors, cat src
+    update() {
+        let elapsedMinutes = Math.floor(this.elapsedSeconds / 60)
+        let remainingSeconds = this.elapsedSeconds - elapsedMinutes * 60
+
+        $('#timerDisplay').text(`${elapsedMinutes.toString().padStart(2, '0')}:${remainingSeconds.toString().padStart(2, '0')}`)
+        $('body').attr('class', this.status)
+        $('#catGraphic').attr('src', `../content/${this.status}-cat-alpha.png`)
+    }
+
+    // Resets all text & all that
+    reset() {
+        $('#timerDisplay').text('00:00')
+        $('#studentNameDisplay').text('')
+
+        $('body').attr('class', 'happy')
+        $('#catGraphic').attr('src', `../content/${this.status}-cat-alpha.png`)
+        $('#catGraphic').addClass('hide')
+
+
+        $('#firstName').prop('disabled', false)
+        $('#lastName').prop('disabled', false)
+
+        $('#firstName').val('')
+        $('#lastName').val('')
+    }
+
+    init() {
+        $('#toggleTimer').on('click', () => {
+            if (this.interval) this.stop()
+            else this.start()
+        })
+        $('#exportCSV').on('click', () => {
+            this.export()
+        })
+    }
+
+    get currentStudent() {
+        return `${this.firstName} ${this.lastName}`
+    }
+    get elapsedSeconds() {
+        if (!this.interval) return 0
+        return Math.round((new Date() - this.startTime) / 1000)
+    }
+    get status() {
+        if (this.elapsedSeconds < 6) return 'happy'
+        else if (this.elapsedSeconds < 12) return 'unhappy'
+        return 'angry'
+    }
+
+    export() {
+        window.open(encodeURI(localStorage.getItem('records')))
+    }
+}
+
+$(document).ready(e => {
+    let a = new Bathroom()
+
+})
